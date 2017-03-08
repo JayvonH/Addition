edc9a703f99c270dd256d19f8eae9f056bbab5f6
JayvonH <jayvon_harris97@hotmail.com>
2017-03-08 16:28:04 -0500

    3rd test

--- add.rb
+++ add.rb
@@ -9,4 +9,7 @@ class TestAddition < Minitest::Test
 	def test_8_plus_42_returns_50
 		assert_equal(50,add(8,42))
 	end
+	def test_9_plus_9_plus_9_returns_27
+		assert_equal(27,adding(9,9,9))
+	end
 end
--- addition.rb
+++ addition.rb
@@ -1,3 +1,7 @@
 def add(num_1, num_2)
 	num_1 + num_2
 end
+
+def adding(num_1, num_2, num_3)
+	num_1 + num_2 + num_3
+end
