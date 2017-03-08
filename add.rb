842ee8db322b79a338ede48b9d19e880b4a44877
JayvonH <jayvon_harris97@hotmail.com>
2017-03-08 16:25:01 -0500

    2nd test

--- add.rb
+++ add.rb
@@ -6,5 +6,7 @@ class TestAddition < Minitest::Test
 	def test_1_plus_1_returns_2
 		assert_equal(2,add(1,1))
 	end
+	def test_8_plus_42_returns_50
+		assert_equal(50,add(8,42))
+	end
 end
-#fasgfsdg
